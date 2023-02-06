a53d82a1c1386a75cedf64ee80452387dea9f50a
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2023-02-06 13:51:15 -0600

    Add initializer to calculator class
    There was a need to capture the operands and operator.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class Calculator
+  def initialize(first_int, operation, second_int)
+    @first_int = first_int
+    @operation = operation
+    @second_int = second_int
+  end
+
   def calculate
   end
+
+  private
+
+  attr_reader :first_int, :operation, :second_int
 end
