3818e5ca99579c9fdc36bb4e770236e4cd400f64
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2023-02-06 14:19:42 -0600

    Add classes for each operation
    This was done to separate the logic for each supported operation

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
+require_relative './operations/add'
+require_relative './operations/divide'
+require_relative './operations/multiply'
+require_relative './operations/subtract'
+
 class Calculator
   class InvalidOperation < ArgumentError; end
 
   OPS = {
-    '+' => 'Add',
-    '-' => 'Subtract',
-    '*' => 'Multiply',
-    '/' => 'Divide'
+    '+' => Add,
+    '-' => Subtract,
+    '*' => Multiply,
+    '/' => Divide
   }.freeze
 
   def initialize(first_int, operation, second_int)
