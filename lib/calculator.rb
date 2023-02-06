90d1e9f0f94a9de8e5fa07313e91bb8e675befec
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2023-02-06 14:11:29 -0600

    Add valid operations map constant
    It was necessary to hardcode the valid operations so that we can raise
    an exception when an invalid operation is passed in.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,16 +1,33 @@
 # frozen_string_literal: true
 
 class Calculator
+  class InvalidOperation < ArgumentError; end
+
+  OPS = {
+    '+' => 'Add',
+    '-' => 'Subtract',
+    '*' => 'Multiply',
+    '/' => 'Divide'
+  }.freeze
+
   def initialize(first_int, operation, second_int)
     @first_int = first_int
-    @operation = operation
     @second_int = second_int
+    @operation = operation
   end
 
   def calculate
+    operation_class
   end
 
   private
 
-  attr_reader :first_int, :operation, :second_int
+  attr_reader :first_int, :second_int, :operation
+
+  def operation_class
+    op = OPS[operation]
+    return op unless op.nil?
+
+    raise InvalidOperation, "'#{operation}' is an invalid operation"
+  end
 end
